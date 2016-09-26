16ed2ae4820b302bcfb3ae64bc4278a5f2784acb
Jeffrey-Young <youngjt@live.unc.edu>
2016-09-26 11:52:27 -0400

    wow I think this might work??

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,9 +8,9 @@ import java.util.ArrayList;
 public class Runner {
 
   public static void main(String[] args) {
-    
-    //read and parse Betas
-    File csvDirectory = new File("/ParseFADTTSterCSV/Betas");
+
+    // read and parse Betas
+    File csvDirectory = new File("Betas");
     BufferedReader betaReader = null;
     BufferedReader rawReader = null;
     ArrayList<Tract> tracts = new ArrayList<Tract>();
@@ -19,45 +18,50 @@ public class Runner {
     for (File csv : csvDirectory.listFiles()) {
       try {
         betaReader = new BufferedReader(new FileReader(csv));
-        tracts.add(new Tract(csv.getName().split("_")[0]));
+        tracts.add(new Tract(csv.getName()));
         tracts.get(index).setArcLengths(betaReader.readLine().split(","));
-        betaReader.readLine(); //intercept
-        betaReader.readLine(); //sex
-        betaReader.readLine(); //age
+        betaReader.readLine(); // intercept
+        betaReader.readLine(); // sex
+        betaReader.readLine(); // age
         tracts.get(index).setExpectedBetas(betaReader.readLine().split(","));
-        
-        //read from Raw Data
-        rawReader = new BufferedReader(new FileReader(new File("/ParseFADTTSterCSV/RawData/" + tracts.get(index).getName() + "_RawData_FA.csv")));
-        rawReader.readLine(); //throw away header line
-        while(rawReader.ready()){ //uhhh maybe this works?
+        // read from Raw Data
+        rawReader = new BufferedReader(new FileReader(new File("RawData/" + tracts.get(index).getName() + "_RawData_FA.csv")));
+        rawReader.readLine(); // throw away header line
+        while (rawReader.ready()) { // uhhh maybe this works?
           tracts.get(index).computeAverageFA(rawReader.readLine().split(","));
-        }        
+        }
         index++;
-      } catch (Exception e){
+      } catch (Exception e) {
         e.printStackTrace();
       }
     }
-    //cleanup
+    // cleanup
     try {
       betaReader.close();
       rawReader.close();
     } catch (IOException e) {
       e.printStackTrace();
     }
-    
-    //write output
-    for (Tract tract : tracts){
+
+    // write output
+    for (Tract tract : tracts) {
       try {
-        FileWriter outputCSV = new FileWriter(new File("/ParseFADTTSterCSV/Output/" + tract.getName() + ".csv"));
-        //write header line
-        outputCSV.write("ArcLength, Average, RelBeta4Exp, ExpCorreBetas, ExpBetas");
-        
-        
-        
+        FileWriter outputCSV = new FileWriter(new File("Output/" + tract.getName() + ".csv"));
+        // write header line
+        outputCSV.write("ArcLength, Average, RelBeta4Exp, ExpCorreBetas, ExpBetas\n");
+
+        // write line
+        for (int i = 0; i < tract.getSize(); i++) {
+          outputCSV.write(tract.getArcLengths().get(i) + "," + tract.getAverageFAs().get(i) + "," + (tract.getExpectedBetas().get(i) * 0.493 * 4) / tract.getAverageFAs().get(i) + ","
+              + (tract.getExpectedBetas().get(i) * 0.493 * 4) + "," + tract.getExpectedBetas().get(i) + "\n");
+        }
+        outputCSV.flush();
+        outputCSV.close();
+
       } catch (IOException e) {
         e.printStackTrace();
       }
     }
-    
+
   }
 }
--- src/Tract.java
+++ src/Tract.java
@@ -1,42 +1,72 @@
 import java.util.ArrayList;
 
 public class Tract {
-  
-  private String _name;
+
+  private String            _name;
   private ArrayList<Double> _arcLengths;
   private ArrayList<Double> _expectedBetas;
-  private ArrayList<Double> _averageFA;
-  
-  public Tract(String name){
-    _name = name;
+  private ArrayList<Double> _averageFAs;
+
+  public Tract(String name) {
+    String temp = "";
+    int i = 0;
+    while (!temp.equals("Betas")) {
+      if (i == 1) {
+        _name = temp;
+      } else {
+        _name += "_" + temp;
+      }
+      temp = name.split("_")[i];
+      i++;
+    }
     _arcLengths = new ArrayList<Double>();
     _expectedBetas = new ArrayList<Double>();
+    _averageFAs = new ArrayList<Double>();
   }
-  
-  //throw away first index (label) and parse the rest into doubles
-  public void setArcLengths(String[] arcLengths){
-    for (int i = 1; i < arcLengths.length; i++){ //start at i = 1 to throw away label
+
+  // throw away first index (label) and parse the rest into doubles
+  public void setArcLengths(String[] arcLengths) {
+    for (int i = 1; i < arcLengths.length; i++) { // start at i = 1 to throw
+                                                  // away label
       _arcLengths.add(Double.parseDouble(arcLengths[i]));
     }
   }
-  
-  public void setExpectedBetas(String[] expectedBetas){
-    for (int i = 1; i < expectedBetas.length; i++){ //start at i = 1 to throw away label
+
+  public void setExpectedBetas(String[] expectedBetas) {
+    for (int i = 1; i < expectedBetas.length; i++) { // start at i = 1 to throw
+                                                     // away label
       _expectedBetas.add(Double.parseDouble(expectedBetas[i]));
     }
   }
-  
-  public void computeAverageFA(String[] averageFA){
+
+  public void computeAverageFA(String[] averageFA) {
     double average = 0;
-    for (int i = 1; i < averageFA.length; i++){ //start at 1 to throw away label
+    for (int i = 1; i < averageFA.length; i++) { // start at 1 to throw away
+                                                 // label
       average += Double.parseDouble(averageFA[i]);
     }
     average /= (averageFA.length - 1);
-    _averageFA.add(average);
+    _averageFAs.add(average);
   }
-  
-  public String getName(){
+
+  public String getName() {
     return _name;
+  }
+
+  public int getSize() {
+    return _arcLengths.size();
+  }
+
+  public ArrayList<Double> getArcLengths() {
+    return _arcLengths;
+  }
+
+  public ArrayList<Double> getAverageFAs() {
+    return _averageFAs;
+  }
+
+  public ArrayList<Double> getExpectedBetas() {
+    return _expectedBetas;
   }
 
 }
